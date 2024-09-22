ecbc680d26d9aebc7fb7a2f4a91064d732e70552
DEEPAK <DEEPAK@DESKTOP-E00HANJ>
2024-09-23 01:24:47 +0530

    Add project files.

---
+++ Products.Api/Program.cs
@@ -0,0 +1,33 @@
+using Learning.Service;
+using Microsoft.EntityFrameworkCore;
+using Products.Api.Database;
+using Products.Api.Endpoints;
+using Products.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+builder.Services.AddStackExchangeRedisCache(options =>
+    options.Configuration = builder.Configuration.GetConnectionString("Cache"));
+
+
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
+}
+
+app.MapProductEndpoints();
+
+app.Run();
---
+++ Products.Api/Service/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Learning.Service
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Product()
+        {
+            try
+            {
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                using (var response = await _httpClient.GetAsync("https://localhost:63499/api/Product?id=1"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        return new OkObjectResult(content);
+                    }
+                    return new NotFoundResult();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+    
+}
